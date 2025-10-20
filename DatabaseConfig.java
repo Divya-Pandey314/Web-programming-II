094c1038938927c22b1712971230e51bde572516
Divya-Pandey314 <havyapandey@gmail.com>
2025-10-20 09:37:40 +0545

    Week 6 Assignment

---
+++ DatabaseConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/girlyme_db?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yourpassword");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
